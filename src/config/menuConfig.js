d7f9a40b66159bfef0ff019c38e10b8b1ad57b2f
Eb <a956895479@gmail.com>
2019-08-02 20:44:29 +0800

    init

---
+++ src/config/menuConfig.js
@@ -0,0 +1,27 @@
+const menuList = [
+    {
+        title: '首页',
+        key: '/admin'
+    },
+    {
+        title: '我的动态',
+        key: '/admin/home'
+    },
+    {
+        title: '众里寻TA',
+        key: '/admin/searchingLove'
+    },
+    {
+        title: '爱豆后援团',
+        key: '/admin/fansClub'
+    },
+    {
+        title: 'Lost&Found',
+        key: '/admin/Lost&Found'
+    },
+    {
+        title: '课后习题解答        ',
+        key: '/admin/FightOfDalao'
+    },
+];
+export default menuList;
